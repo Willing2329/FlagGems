01a9018279cd55b241485003ec91c558e16f734b
qhait <2329292023@qq.com>
2026-09-23 09:33:45 +0000

    [KMCompiler][Nvidia][Hygon][Metax][Thead] Optimize linear

--- src/flag_gems/runtime/backend/_hygon/ops/__init__.py
+++ src/flag_gems/runtime/backend/_hygon/ops/__init__.py
@@ -92,6 +92,7 @@ from .linalg_solve_triangular import (
     linalg_solve_triangular,
     linalg_solve_triangular_out,
 )
+from .linear import linear
 from .log_normal_ import log_normal_
 from .masked_scale import masked_scale
 from .masked_scatter_backward import masked_scatter_backward
@@ -254,6 +255,7 @@ __all__ = [
     "linalg_matrix_power_out",
     "linalg_solve_triangular",
     "linalg_solve_triangular_out",
+    "linear",
     "log_normal_",
     "masked_scale",
     "masked_scatter_backward",
